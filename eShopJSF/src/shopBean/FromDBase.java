7b41ab1f191cb203a167705e2a0268c169e62104
alisaIT <alisa-koksjuk@gmx.de>
2016-04-09 09:45:48 +0100

    aktuellere version

--- eShopJSF/src/shopBean/FromDBase.java
+++ eShopJSF/src/shopBean/FromDBase.java
@@ -1,5 +1,6 @@
 package shopBean;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,134 +10,325 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.*;
+
+import org.bson.Document;
+import org.bson.Document;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.event.ComponentSystemEvent;
+
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.SessionScoped;
+
+
+
 
 @ManagedBean(name = "dbBean", eager = true)
+//@ManagedBean(name = "dbBean")
+
 @SessionScoped
+
 public class FromDBase implements Serializable {
 
+
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-
-	ArrayList<String> user = new ArrayList<String>();
-
-	public ArrayList<String> getUser() {
-		return user;
-	}
-
-	public void setUser(ArrayList<String> user) {
-		this.user = user;
-	}
-
-	public void setEintragImDB(ArrayList<String> eintragImDB) {
-		this.eintragImDB = eintragImDB;
-	}
-
-	ArrayList<String> eintragImDB = new ArrayList<String>();
-	ArrayList<String> bilderList = new ArrayList<String>();
 	
+	int click = 0;
+	double gesamtpreis=0.0;
 
-	
-
-	public void setBilderList(ArrayList<String> bilderList) {
-		this.bilderList = bilderList;
+	public double getGesamtpreis() {
+		return gesamtpreis;
 	}
-	
-	public String  getElementFromEintrag(String i) {
-		
-		int x = 0;
-		Enum test = Enum.valueOf(i);
-		
-		switch (test) {
-		case EINS: x = 1;
-			
-			break;
-		case ZWEI: x = 2;
 
-		break;
-		case DREI: x = 3;
-		break;
-		
-		case VIER: x = 4;
-		break;
-		
-		case FUENF: x = 5;
-		break;
-		
-		case SECHS: x = 6;
-		break;
-		
-		case SIEBEN: x = 7;
-		break;
-		
-		case ACHT: x = 8;
-		default:
-			break;
+	public void setGesamtpreis(double gesamtpreis) {
+		this.gesamtpreis = gesamtpreis;
+	}
+
+	public int getClick() {
+		return click;
+	}
+
+	public void setClick(int click) {
+		this.click = click;
+	}
+
+	Document myDoc = new Document();
+	ArrayList<String> einkaufWagen = new ArrayList<String>();
+
+	public ArrayList<String> getEinkaufWagen() {
+		return einkaufWagen;
+	}
+
+	public void setEinkaufWagen(ArrayList<String> einkaufWagen) {
+		this.einkaufWagen = einkaufWagen;
+	}
+
+	MongoDatabase db;
+	protected DB db1;
+	protected MongoCollection<Document> collection;
+	protected Mongo mongo;
+
+	public ArrayList<String> listeBuecher = new ArrayList<String>();
+
+	public MongoDatabase openDB() throws IOException {
+
+		MongoClient mongoC = new MongoClient("localhost", 27017);
+		MongoDatabase db = mongoC.getDatabase("test3");
+
+		return db;
+	}
+
+	public boolean connectDBCollection(String collectionName) {
+		if (null == collectionName || collectionName.isEmpty()) {
+			return false;
 		}
-		String elem = eintragImDB.get(x);
-		return elem;
-		
+		collection = db.getCollection(collectionName);
+		return true;
 	}
-	
-	public ArrayList<String> getBilderList() {
-		ResultSet rsA = null;
 
-		String mSQL = "select concat('/pictures/', isbn, '.jpg') as url from bücher_gelesen";
+	public BasicDBObject getBasicDBObject() {
+		return new BasicDBObject();
+	}
 
-		try {
-			DBVerbindung verbindung = new DBVerbindung();
-			verbindung.oeffneDB();
+	public void countClick() {
+		click = click + 1;
+	}
 
-			rsA = verbindung.lesen(mSQL);
+	public void calcutate() throws IOException{
+		MongoDatabase db = openDB();
+		countClick();
 
-			while (rsA.next()) {
-				eintragImDB.add(rsA.getString("url"));
-				System.out.println(rsA.getString("url"));
+		FindIterable<Document> iterableI;
+		// db.getCollection("testC4").find().
+
+		// collection testC7 has actual data
+
+		iterableI = getAllDocuments("testC7", db);
+		MongoCursor<Document> iter = iterableI.iterator();
+		int i = 0;
+		int int1;
+		
+		double int1Double=0.0;
+		
+		while (iter.hasNext()) {
+			i++;
+			Document doc = iter.next();
+			System.out.println(doc.toJson());
+
+			System.out.println("Document" + i + ": " + doc.getDouble("price"));
+			int1Double = doc.getDouble("price");
+			// = Integer.toString(int1);
+			while(!doc.isEmpty()){
+				gesamtpreis=gesamtpreis+int1Double;
+				}
 			}
-			
-			rsA.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return bilderList;	
+		System.out.println("gesamtpreis ist" +gesamtpreis);
 	}
+	public void test() {
+		System.out.println("test bean geht!");
+		
+	}
+	public void test2(String test) {
+		System.out.println("test bean geht mit string übergabe!");
+	}
+	
+	public void writeEinkaufToDB(String blub2) throws IOException {
+		MongoDatabase db = openDB();
+		countClick();
 
-	public ArrayList<String> getEintragImDB() {
-		ResultSet rsA = null;
+		FindIterable<Document> iterableI;
+		// db.getCollection("testC4").find().
 
-		String mSQL = "select * from bücher_gelesen";
+		// collection testC7 has actual data
 
-		try {
-			DBVerbindung verbindung = new DBVerbindung();
-			verbindung.oeffneDB();
+		iterableI = getAllDocuments("testC7", db);
+		MongoCursor<Document> iter = iterableI.iterator();
+		int i = 0;
+		int int1;
+		String blub;
+		String int1String;
+		while (iter.hasNext()) {
+			i++;
+			Document doc = iter.next();
+			System.out.println(doc.toJson());
 
-			rsA = verbindung.lesen(mSQL);
+			System.out.println("Document" + i + ": " + doc.getString("isbn"));
+			int1String = doc.getString("isbn");
+			// = Integer.toString(int1);
+			System.out.println(" " + int1String);
 
-			while (rsA.next()) {
-				eintragImDB.add(rsA.getString("titel"));
+			if (blub2.equals(int1String)) {
+				blub = doc.getString("name");
+				System.out.println("das ist blub " + blub);
 				
+				double preis=doc.getDouble("price");
+				gesamtpreis=gesamtpreis+preis;
+				einkaufWagen.add(blub+"        "+preis+" €");
 			}
-			rsA.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+			System.out.println("ges "+gesamtpreis);
+
 		}
-		return eintragImDB;
+
+		/*
+		 * db.getCollection("testC5").insertOne( new Document("Einkauf", new
+		 * Document() .append("user", userMail) .append("userPW", userPw)));
+		 */
+		System.out.println("wurde ausgeführt einfügen user");
+
 	}
-	public enum Enum {
-		
 
-	    EINS,
-	    ZWEI,
-	    DREI,
-	    VIER,
-	    FUENF,
-	    SECHS,
-	    SIEBEN,
-	    ACHT,
-	    NEUN,
-	    ZEHN
-	  
-}
-}
+	public void createUserAndPword() throws IOException, ParseException {
+		MongoDatabase db = openDB();
 
+		String userMail = "mail";
+		String userPw = "pw";
+
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+
+		db.getCollection("testC3")
+				.insertOne(new Document("user", new Document().append("user", userMail).append("userPW", userPw)));
+		System.out.println("wurde ausgeführt einfügen user");
+
+	}
+
+	public String readFirstElemFromCol(String collectionName) {
+		BasicDBObject whereQuery = new BasicDBObject();
+		whereQuery.get(1);
+		String elem = "";
+		db.getCollection(collectionName).find(whereQuery);
+		System.out.println(elem);
+		return elem;
+	}
+
+	public void getSingleField() {
+
+		BasicDBObject allQuery = new BasicDBObject();
+		BasicDBObject fields = new BasicDBObject();
+		fields.put("name", 1);
+		// eq("0", "test")
+
+		myDoc = collection.find(fields).first();
+		System.out.println(myDoc.toJson());
+
+	}
+
+	public ArrayList<String> listeBuecher() throws IOException {
+		// openDB();
+
+		FindIterable<Document> iterable;
+		// iterable= MongoDBVerbindung.getDocuments("testC2", "name",
+		// "mkyong-5", db);
+		// MongoClient mongoC = new MongoClient( "localhost" , 27017 );
+		// MongoDatabase db = mongoC.getDatabase("test3");
+		MongoClient mongoC = new MongoClient("localhost", 27017);
+		MongoDatabase db = mongoC.getDatabase("test3");
+
+		iterable = getAllDocuments("testC2", db);
+		MongoCursor<Document> iter = iterable.iterator();
+		int i = 0;
+		int int1;
+		String int1String;
+		while (iter.hasNext()) {
+			i++;
+			Document doc = iter.next();
+			System.out.println("Document" + i + ": " + doc.getString("name"));
+			int1String = doc.getString("name");
+			// int1String = Integer.toString(int1);
+			System.out.println(" " + int1String);
+			listeBuecher.add(int1String);
+		}
+
+		return listeBuecher;
+	}
+
+	public void enotheTestQuerying(String collName) {
+		FindIterable<Document> iterable = db.getCollection(collName).find();
+
+		iterable.forEach(new Block<Document>() {
+			@Override
+			public void apply(final Document document) {
+				System.out.println(document.toString());
+			}
+		});
+
+	}
+
+	public static FindIterable<Document> getAllDocuments(String collection, MongoDatabase db) {
+
+		FindIterable<Document> iterable = null;
+
+		iterable = getDocuments(collection, null, null, db);
+
+		return iterable;
+	}
+
+	public static FindIterable<Document> getDocuments(String collection, String field, int value, MongoDatabase db) {
+
+		return getDocuments(collection, field, String.valueOf(value), db);
+	}
+
+	public static FindIterable<Document> getDocuments(String collection, String field, String value, MongoDatabase db) {
+
+		FindIterable<Document> iterable = null;
+
+		try {
+			// System.out.println("try to get documents...");
+			if (field == null && value == null) {
+				System.out.println("Search for all documents in collection '" + collection + "'");
+
+				iterable = db.getCollection(collection).find();
+			} else {
+				System.out.println("Search for document with <" + value + "> in field <" + field + ">");
+				iterable = db.getCollection(collection).find(eq(field, value));
+			}
+
+		} catch (Exception e) {
+			System.out.println("FAIL: Something went wrong trying to get documents.");
+			System.out.println(e.toString());
+		}
+
+		// ErrorClass.checkNull(iterable, "Found documents", "No documents
+		// found");
+
+		return iterable;
+	}
+
+	// SHOW [toString()]
+	public static void showDocuments(FindIterable<Document> docs) {
+
+		docs.forEach(new Block<Document>() {
+			public void apply(final Document document) {
+				System.out.println(document);
+			}
+		});
+	}
+
+	public void closeDB() {
+		mongo.close();
+	}
+
+	String userpassword = "";
+	public String getUserpassword() {
+		return userpassword;
+	}
+	
+	
+}
--- eShopJSF/src/shopBean/Main.java
+++ eShopJSF/src/shopBean/Main.java
@@ -1,5 +1,6 @@
 package shopBean;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -7,14 +8,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+		
 		FromDBase t = new FromDBase();
+		
+		
+		
 		ArrayList<String> test= t.getBilderList();
 		
 		Iterator<String>  iter = test.iterator();
 		while (iter.hasNext()) {
 			System.out.println(iter.next());
 		}
+		
+		
 	}
+	
 
 }
